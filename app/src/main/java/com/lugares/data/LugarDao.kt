da4a643659addb3b13e8262907a912f467c322a9
STEVE ARCE <sarce60797@ufide.ac.cr>
2022-03-23 12:27:35 -0600

    Commit de la semana 08 (Firestore Database)

--- app/src/main/java/com/lugares/data/LugarDao.kt
+++ app/src/main/java/com/lugares/data/LugarDao.kt
@@ -1,23 +1,108 @@
 package com.lugares.data
 
+import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.lifecycle.MutableLiveData
+//import androidx.room.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.ktx.Firebase
 import com.lugares.model.Lugar
 
-@Dao
-interface LugarDao {
+class LugarDao {
+
+    private lateinit var codigoUsuario: String
+    private lateinit var firestore: FirebaseFirestore
+
+    init {
+        val usuario = Firebase.auth.currentUser?.email
+        codigoUsuario = "$usuario"
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
+
+    //Para obtener la lista de Lugares
+    fun getLugares(): MutableLiveData<List<Lugar>> {
+        val listaLugares = MutableLiveData<List<Lugar>>()
+        firestore.collection("lugaresApp")
+            .document(codigoUsuario)
+            .collection("misLugares")
+            .addSnapshotListener{ snapshot, e->
+                if (e != null){
+                    return@addSnapshotListener
+                }
+                if(snapshot != null){
+                    val lista = ArrayList<Lugar>()
+                    val lugares = snapshot.documents
+                    lugares.forEach{
+                        val lugar = it.toObject(Lugar::class.java)
+                        if(lugar != null){
+                            lista.add(lugar)
+                        }
+                    }
+                    listaLugares.value = lista
+                }
+
+            }
+        return listaLugares
+    }
+
+    // Salvar Lugar
+    fun saveLugar(lugar: Lugar){
+        val document: DocumentReference
+        if(lugar.id.isEmpty()){
+            document = firestore
+                .collection("lugaresApp")
+                .document(codigoUsuario)
+                .collection("misLugares")
+                .document()
+            lugar.id = document.id
+        } else {
+            document = firestore
+                .collection("lugaresApp")
+                .document(codigoUsuario)
+                .collection("misLugares")
+                .document(lugar.id)
+        }
+        val set = document.set(lugar)
+        set.addOnSuccessListener {
+            Log.d("Add Lugar", "Lugar Agregado")
+        }
+            .addOnCanceledListener {
+                Log.e("Add Lugar", "Lugar NO Agregado")
+            }
+    }
+
+    fun deleteLugar(lugar:Lugar){
+        if(lugar.id.isNotEmpty()){
+            firestore
+                .collection("LugaresApp")
+                .document(codigoUsuario)
+                .collection("misLugares")
+                .document(lugar.id)
+                .delete()
+                .addOnSuccessListener {
+                    Log.d("Add Lugar", "Lugar eliminado")
+
+                }.addOnCanceledListener {
+                    Log.e("Add Lugar", "Lugar NO eliminado")
+                }
+        }
+    }
 
     //Función para obtener la lista de lugares
-    @Query("select * from LUGAR")
-    fun getAllData() : LiveData<List<Lugar>>
+    //@Query("select * from LUGAR")
+    //fun getAllData() : LiveData<List<Lugar>>
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addLugar(lugar: Lugar)
+    //@Insert(onConflict = OnConflictStrategy.IGNORE)
+    // suspend fun addLugar(lugar: Lugar)
 
-    @Update(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun updateLugar(lugar: Lugar)
+    //@Update(onConflict = OnConflictStrategy.IGNORE)
+    //suspend fun updateLugar(lugar: Lugar)
 
-    @Delete
-    suspend fun deleteLugar(lugar: Lugar)
+    //@Delete
+    //suspend fun deleteLugar(lugar: Lugar)
 
 }
--- app/src/main/java/com/lugares/viewmodel/LugarViewModel.kt
+++ app/src/main/java/com/lugares/viewmodel/LugarViewModel.kt
@@ -2,6 +2,7 @@ package com.lugares.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.*
+import com.lugares.data.LugarDao
 import com.lugares.data.LugarDatabase
 import com.lugares.model.Lugar
 import com.lugares.repository.LugarRepository
@@ -11,21 +12,25 @@ import kotlinx.coroutines.launch
 class LugarViewModel(application: Application)
     : AndroidViewModel(application) {
 
-    val getAllData : LiveData<List<Lugar>>
+    val getAllData : MutableLiveData<List<Lugar>>
 
-    private val repository: LugarRepository
+    private val repository: LugarRepository = LugarRepository(LugarDao())
     init {
-        val lugarDao = LugarDatabase.getDatabase(application).lugarDao()
-        repository = LugarRepository(lugarDao)
+        //val lugarDao = LugarDatabase.getDatabase(application).lugarDao()
+        // repository = LugarRepository(lugarDao)
         getAllData = repository.getAllData
     }
-    fun addLugar(lugar: Lugar) {
-        viewModelScope.launch(Dispatchers.IO) {repository.addLugar(lugar)}
+    suspend fun addLugar(lugar: Lugar) {
+        //viewModelScope.launch(Dispatchers.IO) {repository.addLugar(lugar)}
+        repository.addLugar(lugar)
     }
-    fun updateLugar(lugar: Lugar) {
-        viewModelScope.launch(Dispatchers.IO) {repository.updateLugar(lugar)}
+    suspend fun updateLugar(lugar: Lugar) {
+        //viewModelScope.launch(Dispatchers.IO) {repository.updateLugar(lugar)}
+        repository.addLugar(lugar)
     }
-    fun deleteLugar(lugar: Lugar){
-        viewModelScope.launch(Dispatchers.IO) {repository.deleteLugar(lugar)}
+    suspend fun deleteLugar(lugar: Lugar){
+        //viewModelScope.launch(Dispatchers.IO) {repository.deleteLugar(lugar)}
+        repository.deleteLugar(lugar)
     }
+
 }
